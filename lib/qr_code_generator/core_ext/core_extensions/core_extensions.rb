8dd7b553f128dd273abc11d1accd2a92920bf733
Scott W. Bradley <scottwb@scottwb.net>
2009-10-04 00:43:33 -0700

    Added module, class, and method comment documentation, and added a convenience module method for encoding to HTML format.

--- lib/qr_code_generator/core_ext/core_extensions/core_extensions.rb
+++ lib/qr_code_generator/core_ext/core_extensions/core_extensions.rb
@@ -1,5 +1,23 @@
 module QRCodeGenerator
+  # This module can be included on core classes such as String, Hash, Array,
+  # etc. to extend them with these convenience methods for generating
+  # QR Codes on them.
   module CoreExtensions
+
+    # Converts this object to a QRCodeGenerator::QRCode instance that
+    # represents this object encoded as a QRCode.
+    #
+    # ====Parameters
+    #
+    # +options+::
+    #   An optional Hash of options to affect the QR Code generation.
+    #   See the QRCodeGenerator "QR Code Options" documentation for details.
+    #
+    # ====Returns
+    #
+    # A QRCodeGenerator::QRCode instance that represents this object
+    # encoded as a QRCode.
+    #
     def to_qr(options = {})
       QRCodeGenerator::QRCode.new(self, options)
     end
--- lib/qr_code_generator/qr_code_generator.rb
+++ lib/qr_code_generator/qr_code_generator.rb
@@ -2,47 +2,276 @@ require 'rqrcode'
 require 'json'
 require 'RMagick'
 
-# The QRCodeGenerator class contains...TODO: document this.
-#
+
 # TODO:
-#   [X] Turn into class model.
-#   [X] Make image margin configurable.
-#   [X] Add to_qr methods to Strings, Hash, and Array.
-#   [X] Maybe add to_qr to ActiveRecord::Base
-#   [X] Check in to github.
-#   [X] Add HTML table generation.
-#   [ ] Figure out a better way to compute the min size that trial and error.
-#   [ ] Cleanup REVISITs
-#   [ ] Write comment documentation.
+#   [ ] Add ability to overlay an image.
+#   [ ] Figure out a better way to compute the min QR size than trial and error.
+#   [ ] Optimize the to_image generation of pixel data.
+#   [ ] Write license, copyright, README, INSTALL, etc. docs.
 #   [ ] Add rdoc/ri tasks.
 #   [ ] Write wiki documentation.
 #   [ ] Build runit test cases and/or maybe even rspec for the plugin.
-#   [ ] Make into a plugin gem. Make it have a dependency on rqrcode.
+#   [ ] Make into a plugin gem with dependencies on:
+#         * RQRCode
+#         * RMagick
+#         * JSON
 #   [ ] Submit to rubyforge?
+
+
+# The QRCodeGenerator module contains the QRCodeGenerator::QRCode class
+# and a few module methods provided for convenience, for converting
+# data objects into QR Codes.
+#
+#
+# ==Example Usage
+#
+#   QRCodeGenerator.encode_to_image_file(
+#     "hello world",
+#     "hello.png",
+#     :size => 500
+#   )
+#
+#   {:hello => "world"}.to_qr.to_html(:size => 500)
+#
+#
+# ==Core Extensions
+#
+# The QRCodeGenerator::CoreExtensions#to_qr method is added to core
+# classes such as String, Hash, and Array, as well as ActiveRecord::Base.
+# This allows you to simply do something like:
+#
+#   "hello world".to_qr
+#
+# to create a QRCodeGenerator::QRCode instance that you can then render
+# to image or HTML with methods such as QRCodeGenerator::QRCode#to_image
+# and QRCodeGenerator::QRCode#to_html.
+#
+#
+# ==QR Code Options
+#
+# The QR Code generation methods that take "QR Code Options" can use the
+# following options:
+#
+#   :min_size => The minimum size (QR Code type number) to generate the
+#                QR Code. This size will be used unless it is not big enough
+#                to hold the specified data, in which case, the smallest
+#                size that is big enough to hold it will be used. This
+#                Must be a number from 1 to 40. By default it is set to 1.
+#
+#   :level => The error correction level to encode the QR Code with. This
+#             defaults to :h and can be one of the following values:
+#
+#               :l - 7% of the code can be restored.
+#
+#               :m - 15% of the code can be restored.
+#
+#               :q - 25% of the code can be restored.
+#
+#               :h - 30% of the code can be restored.
+#
+#   :encoding => When the data being encoded is not a String, this option
+#                specifies what encoding to use to convert the data to
+#                a Strings. This defaults to :json, and may be one of the
+#                following values:
+#
+#                  :json - The data object is encoded using to_json.
+#
+#                  :xml - The data object is encoded using to_xml.
+#
+#                  :yaml - The data object is encoded using to_yaml.
+#
+#                  :marshal - The data object is encoded using Marshal.dump
+#                             and then Base64.encode64.
+#
+#                  :string - The data object is encoded using to_s.
+#
+#
+# ==Image Options
+#
+# The QR Code image generation methods that take "Image Options" can use
+# the following options:
+#
+#   :size => The size, in pixels to generate the image to. This includes only
+#            the actual QR Code image. The width and height of the image will
+#            set to this value. By default, if this is unspecified, the
+#            image will be generated as small as possible, using one pixel
+#            per QR Code module.
+#
+#   :margin => The number of modules to add, in addition to :size, as a margin
+#              around the image. This defaults to 4, as the QR Code
+#              specification requires that there is a 4-module border around
+#              the QR Code image. Note that this means the actual image
+#              that is generated will be larger than the specified :size by
+#              this margin.
+#
+#   :format => The image format to generate the image in. This defaults to
+#              'png', and may be any of the format values supported by
+#              RMagick, such as 'jpg', 'gif', etc.
+#
+#
+# ==HTML Options
+#
+# The QR Code HTML generation methods that take "HTML Options" can use
+# the following options:
+#
+#   :size => The approximate size, in pixels, to generate the HTML table
+#            to. The generated HTML table will be approximately this wide
+#            and high. This only includes the QR Code part of the output,
+#            not the margin, and is only approximate since the number of
+#            modules required to fit into this size might not be evenly
+#            divisible into the :size. By default this is 200.
+#
+#   :margin => The number of modules to add, in addition to :size, as a
+#              margin around the HTML table. This defaults to 4, as the
+#              QR Code specification requires that there is a 4-module
+#              border around the QR Code image. Note that this means the
+#              actual HTML table that is generated will be larger than
+#              the specified :size by this margin.
 #
 module QRCodeGenerator
+  VERSION = "0.9.0"
+
+
+  ############################################################
+  # Default Options
+  ############################################################
+  
+  # Default values for QR Code options.
+  DEFAULT_QR_OPTIONS = {
+    :min_size => 1,
+    :level    => :h,
+    :encoding => :json
+  }
+  
+  # Default values for QR Code image output options.
+  DEFAULT_IMG_OPTIONS = {
+    :margin => 4,    # QR Code spec requries min. 4 "module" white margin.
+    :size   => nil,  # Default to no scaling.
+    :format => 'png'
+  }
+
+  # Default values for QR Code HTML output options.
+  DEFAULT_HTML_OPTIONS = {
+    :margin => 4,   # QR Code spec requires min. 4 "module" white margin.
+    :size   => 200
+  }
+
+
+  ############################################################
+  # QRCodeGenerator Module Methods
+  ############################################################
+
+  # Converts the specified +data+ into a QR Code string that represents
+  # the QR Code as ASCII art so to speak, using an X for a black module
+  # and a space for a white module.
+  #
+  # ====Parameters
+  #
+  # +data+::
+  #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+  #
+  # +options+::
+  #   An optional Hash of options to affect the QR Code generation.
+  #   See "QR Code Options".
+  #
+  # ====Returns
+  #
+  # A String that represents the +data+ encoded to a QR Code.
+  #
+  def QRCodeGenerator.encode_to_string(data, options = {})
+    QRCode.new(data, options).to_s
+  end
+
+  # Converts the specified +data+ into a Magick::Image image object that
+  # is a rendering of the +data+ encoded as a QR Code.
+  #
+  # ====Parameters
+  #
+  # +data+::
+  #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+  #
+  # +options+::
+  #   An optional Hash of options to affect the QR Code image generation.
+  #   These may include "QR Code Options" and "Image Options".
+  #
+  # ====Returns
+  #
+  # A Magick::Image object that represents the +data+ encoded to a
+  # QR Code image.
+  #
+  def QRCodeGenerator.encode_to_image(data, options = {})
+    QRCode.new(data, options).to_image(options)
+  end
+  
+  # Converts the specified +data+ into binary image data that is a rendering
+  # of the +data+ encoded as a QR Code.
+  #
+  # ====Parameters
+  #
+  # +data+::
+  #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+  #
+  # +options+::
+  #   An optional Hash of options to affect the QR Code image generation.
+  #   These may include "QR Code Options" and "Image Options".
+  #
+  # ====Returns
+  #
+  # A String that contains the generated binary image data representing
+  # the +data+ encoded to a QR Code image.
+  #
+  def QRCodeGenerator.encode_to_image_blob(data, options = {})
+    QRCode.new(data, options).to_image_blob(options)
+  end
+  
+  # Converts the specified +data+ into an image file that is a rendering
+  # of the +data+ encoded as a QR Code.
+  #
+  # ====Parameters
+  #
+  # +data+::
+  #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+  #
+  # +filename+::
+  #   The path to the file to write the generated image to.
+  #
+  # +options+::
+  #   An optional Hash of options to affect the QR Code image generation.
+  #   These may include "QR Code Options" and "Image Options".
+  #
+  # ====Returns
+  #
+  # The Magick::Image object that was written to the specified file.
+  #
+  def QRCodeGenerator.encode_to_image_file(data, filename, options = {})
+    QRCode.new(data, options).to_image_file(filename, options)
+  end
+  
+  # Converts the specified +data+ into an HTML table that is a rendering
+  # of the +data+ encoded as a QR Code.
+  #
+  # ====Parameters
+  #
+  # +data+::
+  #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+  #
+  # +options+::
+  #   An optional Hash of options to affect the QR Code image generation.
+  #   These may include "QR Code Options" and "HTML Options".
+  #
+  # ====Returns
+  #
+  # A String containing the generated HTML.
+  #
+  def QRCodeGenerator.encode_to_html(data, options = {})
+    QRCode.new(data, options).to_html(options)
+  end
+
+
+  # An instance of the QRCodeGenerator::QRCode class represents a QR Code
+  # that has been encoded from a data object. It supports methods
+  # for rendering it in various ways.
   class QRCode
-
-    ############################################################
-    # Configuration
-    ############################################################
-    DEFAULT_QR_OPTIONS = {
-      :min_size => 1,
-      :level    => :h,
-      :encoding => :json
-    }
-
-    DEFAULT_IMG_OPTIONS = {
-      :margin => 4,    # QR Code spec requries min. 4 "module" white margin.
-      :size   => nil,  # Default to no scaling.
-      :format => 'png'
-    }
-    
-    DEFAULT_HTML_OPTIONS = {
-      :margin => 4,   # QR Code spec requires min. 4 "module" white margin.
-      :size   => 200
-    }
-
 
     ############################################################
     # Constants
@@ -64,29 +293,24 @@ module QRCodeGenerator
 
 
     ############################################################
-    # Class Methods
-    ############################################################
-    def self.encode_to_string(data, options = {})
-      self.new(data, options).to_s
-    end
-
-    def self.encode_to_image(data, options = {})
-      self.new(data, options).to_image(options)
-    end
-
-    def self.encode_to_image_blob(data, options = {})
-      self.new(data, options).to_image_blob(options)
-    end
-
-    def self.encode_to_image_file(data, filename, options = {})
-      self.new(data, options).to_image_file(filename, options)
-    end
-                                          
-  
-    ############################################################
     # Instance Methods
     ############################################################
-
+    
+    # Creates a new QRCode instance from the given +data+.
+    #
+    # ====Parameters:
+    #
+    # +data+::
+    #   The data object, e.g.: a String, Hash, Array, etc. to be encoded.
+    #
+    # +options+::
+    #   An optional Hash of options to affect the QR Code generation.
+    #   See "QR Code Options".
+    #
+    # ====Returns
+    #
+    # The new QRCode instance.
+    #
     def initialize(data, options = {})
       @data   = data
       @images = {}
@@ -133,11 +357,34 @@ module QRCodeGenerator
       @width  = @qr.module_count
       @height = @width
     end
-
+    
+    # Renders this QRCode as an "ASCII art" String, using an X for a black
+    # module and a space for a white module.
+    #
+    # ====Parameters
+    #
+    # None.
+    #
+    # ====Returns
+    #
+    # The String-rendering of this QRCode.
+    #
     def to_s
       @qr.to_s
     end
-
+    
+    # Renders this QRCode as an HTML table.
+    #
+    # ====Parameters
+    #
+    # +options+::
+    #   An optional Hash containing options that affect the HTML generation.
+    #   See "HTML Options".
+    #
+    # ====Returns
+    #
+    # A String containing the generated HTML.
+    #
     def to_html(options = {})
       html = nil
 
@@ -179,7 +426,19 @@ module QRCodeGenerator
 
       return html
     end
-
+    
+    # Renders this QRCode as a Magick::Image object.
+    #
+    # ====Parameters
+    #
+    # +options+::
+    #   An optional Hash containing options that affect the image generation.
+    #   See "Image Options".
+    #
+    # ====Returns
+    #
+    # A Magick::Image instance containing the rendered image.
+    #
     def to_image(options = {})
       image = nil
 
@@ -254,6 +513,19 @@ module QRCodeGenerator
       return image
     end
 
+    # Renders this QRCode as binary image data.
+    #
+    # ====Parameters
+    #
+    # +options+::
+    #   An optional Hash containing options that affect the image generation.
+    #   See "Image Options".
+    #
+    # ====Returns
+    #
+    # A String that contains the generated binary image data representing
+    # this QRCode.
+    #
     def to_image_blob(options = {})
       # Set the default options if they are unspecified.
       opts = DEFAULT_IMG_OPTIONS.merge(options)
@@ -263,6 +535,22 @@ module QRCodeGenerator
       end
     end
 
+    # Renders this QRCode to an image file.
+    #
+    # ====Parameters
+    #
+    # +filename+::
+    #   The path to the file to write the generated image to. The extension
+    #   of this filename is used to determine the image file format.
+    #
+    # +options+::
+    #   An optional Hash containing options that affect the image generation.
+    #   See "Image Options".
+    #
+    # ====Returns
+    #
+    # The Magick::Image object that was written to the specified file.
+    #
     def to_image_file(filename, options = {})
       self.to_image(options).write(filename)
     end
